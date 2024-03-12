21b0fed907550ee934991d8348123d6bdf29dde6
jacopo-derrico <derricoj.design@gmail.com>
2024-03-12 17:54:06 +0100

    instal npm & sass

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,6 +8,8 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
+    @vite('resources/js/app.js')
+
     <title>Home</title>
 </head>
 <body>
